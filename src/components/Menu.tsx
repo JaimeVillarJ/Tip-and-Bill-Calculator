c1003560ce1946a5badb05bac73eaae89ff4b330
JaimeVillarJ <jaimevvvjjj@gmail.com>
2025-09-13 22:13:37 -0500

    restore .env

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,10 +1,11 @@
-import axios from "axios";
-import { Drink } from "./Drink";
-import { useEffect, useState } from "react";
+import axios from "axios"
+import { Drink } from "./Drink"
+import { useEffect, useState } from "react"
 import type { drink } from "../types/types";
 import { Bill } from "../components/Bill";
 
 export const Menu = () => {
+
   const [menu, setMenu] = useState<drink[]>([]);
   const [loading, setLoading] = useState(true);
   const [consumed, setConsumed] = useState<drink[]>([]);
@@ -12,43 +13,41 @@ export const Menu = () => {
   const API_LINK = import.meta.env.VITE_DEV_API_LINK;
 
   useEffect(() => {
-    if (!API_LINK) {
-      console.error("API_LINK is undefined");
+    axios.get(`${API_LINK}/api/menu`)
+    .then(response => {
+      setMenu(response.data);
       setLoading(false);
-      return;
-    }
-
-    axios
-      .get(`${API_LINK}/api/menu`)
-      .then((response) => {
-        const data = response.data;
-        // Aseguramos que siempre sea un array
-        setMenu(Array.isArray(data) ? data : data.menu || []);
-      })
-      .catch((error) => {
-        console.error("Error fetching menu:", error);
-      })
-      .finally(() => setLoading(false));
+    })
+    .catch(error => {
+      console.error("Error to get drinks", error);
+      setLoading(false);
+    });
   }, []);
 
   return (
-    <div className="flex m-3.5 gap-2 p-4 justify-center">
-      <div className="flex flex-col gap-2 mr-30.5 mb-10">
-        <h1 className="font-black text-3xl mb-5">Menú</h1>
-        {loading ? (
-          <p>loading...</p>
-        ) : (
-          menu.map((item) => (
-            <Drink
-              key={item.id}
-              item={item}
-              drinks={consumed}
-              setConsumed={setConsumed}
-            />
-          ))
-        )}
+    <>
+      <div className="flex m-3.5 gap-2 p-4 justify-center">
+        <div className="flex flex-col gap-2 mr-30.5 mb-10">
+          <h1 className="font-black text-3xl mb-5">Menú</h1>
+          {
+            loading ? 
+              <p>loading...</p> 
+            :
+            menu.map((item) => (
+              <Drink
+                item = {item}
+                drinks={consumed}
+                setConsumed = {setConsumed}
+              />
+            ))
+          }    
+        </div>
+          <Bill 
+            consume={consumed}
+            setConsumed={setConsumed}
+          /> 
       </div>
-      <Bill consume={consumed} setConsumed={setConsumed} />
-    </div>
-  );
-};
+    </>
+    
+  )
+}
